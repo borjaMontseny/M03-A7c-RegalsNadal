f91d61cf8876fdc23f670e956e74e61d03f2c470
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2023-12-01 20:30:49 +0100

    COmmit

--- src/Llibre.java
+++ src/Llibre.java
@@ -1,11 +1,25 @@
 
 public class Llibre {
+
+	// PROPIEDADES
 	String titol;
 	String autor;
 	int numeroPagines;
 	double llarg;
 	double ample;
 	int paginaActual = 0;
+
+	// CONSTRUCTORES
+	public Llibre(String titol, String autor, int numeroPagines, double llarg, double ample, int paginaActual) {
+		this.titol = titol;
+		this.autor = autor;
+		this.numeroPagines = numeroPagines;
+		this.llarg = llarg;
+		this.ample = ample;
+		this.paginaActual = paginaActual;
+	}
+
+	// MÉTODOS
 
 	void setMarcador(int pagina) {
 		this.paginaActual = pagina;
--- src/Perfum.java
+++ src/Perfum.java
@@ -1,11 +1,72 @@
 
 public class Perfum {
+
+	// PROPIEDADES
 	String nom;
 	String marca;
 	double mililitres;
 	double altura;
 	String olor;
 	double mililitresRestants = mililitres;
+
+	// CONSTRUCTORES
+	public Perfum(String nom, String marca, double mililitres, double altura, String olor, double mililitresRestants) {
+		this.nom = nom;
+		this.marca = marca;
+		this.mililitres = mililitres;
+		this.altura = altura;
+		this.olor = olor;
+		this.mililitresRestants = mililitresRestants;
+	}
+
+	// MÉTODOS
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getMililitres() {
+		return mililitres;
+	}
+
+	public void setMililitres(double mililitres) {
+		this.mililitres = mililitres;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public String getOlor() {
+		return olor;
+	}
+
+	public void setOlor(String olor) {
+		this.olor = olor;
+	}
+
+	public double getMililitresRestants() {
+		return mililitresRestants;
+	}
+
+	public void setMililitresRestants(double mililitresRestants) {
+		this.mililitresRestants = mililitresRestants;
+	}
 
 	void perfumar() {
 		this.mililitresRestants -= 0.1;
